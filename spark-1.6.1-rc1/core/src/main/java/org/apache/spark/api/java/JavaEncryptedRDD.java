c43afea6bd828dfbf8f94a3b47cda400af7ebbd5
Steven Stulga <sjstulga@vt.edu>
2016-03-16 15:47:13 -0400

    Fixing some bugs and typos in JavaEncryptedRDD.java

--- spark-1.6.1-rc1/core/src/main/java/org/apache/spark/api/java/JavaEncryptedRDD.java
+++ spark-1.6.1-rc1/core/src/main/java/org/apache/spark/api/java/JavaEncryptedRDD.java
@@ -1,14 +1,15 @@
-package org.apache.spark.api.java
+package org.apache.spark.api.java;
 
-import org.apache.spark.api.java.javaRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.rdd.RDD;
 
-public class JavaEncryptedRDD<T> extend JavaRDD<T>{
-  public class JavaEncryptedRDD(RDD<T> rdd, scala.reflect.ClassTag<T> classTag) {
+public class JavaEncryptedRDD<T> extends JavaRDD<T> {
+  public JavaEncryptedRDD(RDD<T> rdd, scala.reflect.ClassTag<T> classTag) {
     super(rdd, classTag);
   }
 
   @Override
-  public void saveAsTextFile(path) {
+  public void saveAsTextFile(String path) {
     super.saveAsTextFile("/tmp" + path);
   }
 }
